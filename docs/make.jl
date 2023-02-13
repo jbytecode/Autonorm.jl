fd245a207add30f0faec5d36e04f4c0e8ddef07f
Mehmet Hakan Satman <mhsatman@gmail.com>
2023-02-13 19:49:10 +0300

    add docs folder

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter, Autonorm
+
+
+makedocs(
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        collapselevel = 2,
+        # assets = ["assets/favicon.ico", "assets/extra_styles.css"],
+    ),
+    sitename = "Autonorm.jl",
+    authors = "Mehmet Hakan Satman <mhsatman@gmail.com>, Emre Akadal <emreakadal@gmail.com>",
+    pages = [
+        "Main Functionality" => "main.md",
+        "Datasets" => "datasets.md",
+    ],
+)
