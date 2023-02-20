2312636b8c8dbf683e3e026503bf4ff19df2e40d
Mehmet Hakan Satman <mhsatman@gmail.com>
2023-02-20 22:21:13 +0300

    update testcode

--- testcode.jl
+++ testcode.jl
@@ -1,9 +1,10 @@
 # Usage
 # julia> ]
 # Pkg] develop .
-# julia> include("testcode.jl")
 # using Revise
 # using Autonorm
+
+# julia> include("testcode.jl")
 
 # julia code ...
 # in each loading of testcode.jl, package will be recompiled if there is a change.
